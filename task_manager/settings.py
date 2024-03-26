63d99b46be061d323e4e2e415778bc92f841cf14
Artur Medvedev <varouser@gmail.com>
2024-03-27 04:36:31 +0500

    Get SECRET_KEY with dotenv

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 PROJECT_DIR = Path(__file__).resolve().parent
@@ -20,6 +22,8 @@ BASE_DIR = PROJECT_DIR.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
