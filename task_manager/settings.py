6ee9092b66c7e15f387c7a84b4bb27adeeed7271
Artur Medvedev <varouser@gmail.com>
2024-03-27 04:34:10 +0500

    Add secret key to .env

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = PROJECT_DIR.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o7pe8rf+r&-uiskf!krml!tm!sy&dc(8ew%%!ur)k1yxnt5t4('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
