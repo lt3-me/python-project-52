0583bbe2a9378094325f314fead26872ddbe4e00
Artur Medvedev <varouser@gmail.com>
2024-03-28 03:17:29 +0500

    Create test file

---
+++ tests/test_crud.py
@@ -0,0 +1,12 @@
+# from os import path
+# import pytest
+# from django.test import TestCase
+# from task_manager import models
+
+# def get_fixture_filepath(filename):
+#     return path.join(path.dirname(__file__), 'fixtures', filename)
+
+
+# @pytest.fixture
+# def test_model_data():
+#     pass
