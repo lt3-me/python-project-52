f73ecd2149ae2bb49f5b119cdb6b48e56c64aaeb
Artur Medvedev <varouser@gmail.com>
2024-03-27 06:32:49 +0500

    Add server host to ALLOWED_HOSTS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'webserver']
+ALLOWED_HOSTS = [
+    '127.0.0.1', 'localhost', 'task-manager-ubxr.onrender.com', 'webserver']
 
 
 # Application definition
