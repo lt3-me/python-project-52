ac708aefb638848576b953188d8384e0263fa547
Artur Medvedev <varouser@gmail.com>
2024-03-28 05:55:20 +0500

    Change import

--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.views import View
-from . import models
+from task_manager.user.models import User
 
 
 class UsersView(View):
 
     def get(self, request, *args, **kwargs):
-        users = models.User.objects.all()
+        users = User.objects.all()
         return render(request, 'users/index.html', context={
             'users': users})
