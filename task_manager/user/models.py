500a1972b900503a54e2271ffd34e4ed727162ef
Artur Medvedev <varouser@gmail.com>
2024-03-28 07:32:00 +0500

    Update User model

--- task_manager/user/models.py
+++ task_manager/user/models.py
@@ -4,11 +4,16 @@ from django.contrib.auth.models import AbstractBaseUser
 
 class User(AbstractBaseUser):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30, unique=True)
-    full_name = models.CharField(max_length=60)
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    password = models.CharField(max_length=30)
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_short_name(self):
+        return self.first_name
 
     def __str__(self):
         return self.name
@@ -17,4 +22,4 @@ class User(AbstractBaseUser):
         db_table = 'users'
         app_label = 'task_manager'
 
-    USERNAME_FIELD = 'name'
+    USERNAME_FIELD = 'username'
