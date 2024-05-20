568ca72bbb031e8b7b8589ee9b469a7db0edd53d
Artur Medvedev <varouser@gmail.com>
2024-05-21 00:15:09 +0500

    Fix url

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -24,6 +24,6 @@ from django.views.generic import TemplateView, RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('favicon.ico/', RedirectView.as_view(url='/static/favicon.ico')),
-    path('users/', include('task_manager.user.urls')),
+    path('users/', include('task_manager.users.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
