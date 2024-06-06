4dd04f664e9c5b66b24c2da2b7cf37f9af2fb6e2
Artur Medvedev <varouser@gmail.com>
2024-06-06 18:41:39 +0500

    Add statuses CRUD

---
+++ task_manager/statuses/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import StatusesView, CreateStatusView, \
+                    UpdateStatusView, DeleteStatusView
+
+urlpatterns = [
+    path('', StatusesView.as_view(), name='statuses'),
+    path('create/', CreateStatusView.as_view(), name='create_status'),
+    path('<int:pk>/update/', UpdateStatusView.as_view(), name='update_status'),
+    path('<int:pk>/delete/', DeleteStatusView.as_view(), name='delete_status'),
+]
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Status
+# from .forms import UserDataForm
+# from task_manager.mixins import LoginCheckMixin, UserCheckMixin
+# from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse_lazy
+from task_manager.mixins import LoginCheckMixin
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 # Create your views here.
+class StatusesView(LoginCheckMixin, ListView):
+    template_name = 'statuses/index.html'
+    model = Status
+    context_object_name = 'statuses'
+    extra_context = {
+        'title': _('Statuses')
+    }
+
+
+class CreateStatusView(
+        LoginCheckMixin,
+        SuccessMessageMixin,
+        CreateView):
+    fields = ('name',)
+    model = Status
+    template_name = 'form.html'
+    success_url = reverse_lazy('statuses')
+    success_message = _('Status has been created successfully.')
+    extra_context = {
+        'title': _('Create status'),
+        'button_text': _('Create')
+    }
+
+
+class UpdateStatusView(
+        LoginCheckMixin,
+        SuccessMessageMixin,
+        UpdateView):
+    fields = ('name',)
+    model = Status
+    template_name = 'form.html'
+    success_url = reverse_lazy('statuses')
+    success_message = _('Status has been edited successfully.')
+    extra_context = {
+        'title': _('Edit status'),
+        'button_text': _('Edit')
+    }
+
+
+class DeleteStatusView(
+        LoginCheckMixin,
+        SuccessMessageMixin,
+        DeleteView):
+    template_name = 'statuses/delete.html'
+    model = Status
+    success_url = reverse_lazy('statuses')
+    success_message = _('Status has been successfully deleted')
+    extra_context = {
+        'title': _('Delete status'),
+        'button_text': _('Yes, delete'),
+    }
