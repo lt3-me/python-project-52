ee1b9cdd1ba2d1b03fb307e7054223c14cd37be2
Artur Medvedev <varouser@gmail.com>
2024-06-29 11:25:51 +0500

    Add test for task delete protection

---
+++ task_manager/tasks/tests/test_task_delete_protection.py
@@ -0,0 +1,26 @@
+from django.urls import reverse_lazy
+from task_manager.tests.base import BaseTestCase
+from django.utils.translation import gettext_lazy as _
+
+from task_manager.users.models import User
+from task_manager.tasks.models import Task
+
+
+class DeleteProtectionTest(BaseTestCase):
+    fixtures = ['task_manager/tests/fixtures/db_tasks.json']
+
+    def test_delete_task_of_another_user(self):
+        new_user = User.objects.create_user('test_username')
+        self.client.force_login(user=new_user)
+        task_id = Task.objects.first().id
+        response = self.client.post(
+            reverse_lazy(
+                'delete_task',
+                kwargs={'pk': task_id}
+            ), follow=True
+        )
+        self.assertRedirects(response, reverse_lazy('tasks'))
+        self.assertContains(
+            response,
+            _('Task can be deleted only by the creator.'))
+        self.assertTrue(Task.objects.filter(id=task_id).exists())
